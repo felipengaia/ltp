55a4ccd87b10f0aa9f60f9bfeb60dc2ccd50285d
Felipe Nunes Gaia <39844375+felipengaia@users.noreply.github.com>
2023-09-12 11:37:09 -0300

    Novos exercícios
    Verifica Inteiro
    Maior de Idade

--- Classes/src/gui/Hello.java
+++ Classes/src/gui/Hello.java
@@ -3,8 +3,8 @@ package gui;
 import java.util.Scanner;
 
 public class Hello {
-
-	public static void main(String[] args) {
+	
+	public void leTipos() {
 		// declaração de variáveis
 		// sempre no início - Boa prática!
 		// inicializar as variáveis - Boa prática!
@@ -28,6 +28,56 @@ public class Hello {
 		// saída de dados
 		System.out.println("Olá Mundo! char: " + a);
 		System.out.println("Olá Mundo! int: " + x);
-		System.out.println("Olá Mundo! double: " + z);
+		System.out.println("Olá Mundo! double: " + z);		
+	}
+	
+	public void verificaInteiro() {
+		// declarar um inteiro
+		int i = 0;
+		
+		// le um inteiro do teclado
+		Scanner in = new Scanner(System.in);
+		i = in.nextInt();
+
+		// verifica se é par
+		if(i % 2 == 0) {
+			System.out.print("par ");
+		}
+		// senao
+		else {
+			System.out.print("impar ");			
+		}
+		
+		// verifica se é positivo
+		if(i > 0) {
+			System.out.println("positivo");			
+		}
+		// verifica se é negativo
+		if (i < 0) {
+			System.out.println("negativo");
+		}
+	}
+	
+	public void maiorIdade() {
+		// declarar a variavel e inicializa
+		double idade = 0.0;
+		
+		// le a idade
+		Scanner in = new Scanner(System.in);
+		idade = in.nextDouble();
+		
+		// se idade maior ou igual a 18
+		if(idade >= 18.0) {
+			System.out.println("maior de idade");
+		}
+		// else
+		else {
+			System.out.println("menor de idade");
+		}
+	}
+
+	public static void main(String[] args) {
+		Hello h = new Hello();
+		h.maiorIdade();	
 	}
 }
